282feed2e56648bb8425633d5304b7d20da2985e
Jacky <keout1706@gmail.com>
2026-03-22 23:50:28 +0800

    合併完成

--- guess_game.py
+++ guess_game.py
@@ -6,17 +6,20 @@ Created on Mon Jan 26 20:08:05 2026
 """
 
 import random
-
 start,end=1,100
-
+count=10
 # 產生亂數
 x=random.randint(start, end)    
 # 提示答案
 #print(x)
 
-for i in range(10):
-    y=int(input("請猜一個數字(%d~%d):"%(start, end)))
+for i in range(count):
+    y=int(input(f"第{i+1}/{count}次，請猜一個數字({start}~{end}):"))
     
+if y<start or y>end:
+    print("不要亂猜~~~~!")
+    continue
+
     # 猜對 + break
     if x==y:
         print("恭喜猜對!")
@@ -24,8 +27,12 @@ for i in range(10):
     
     # 猜錯
     if x>y:
+        if y>start:
+            start=y+1
         print("猜大一點")
     else:
+        if y<end:
+            end=y-1
         print("猜小一點")
    
 
